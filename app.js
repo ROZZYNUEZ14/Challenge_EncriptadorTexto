64e2a4764edcd5359b55881a3e57ef1a7342a8ab
ROZZYNUEZ14 <palacinrossy@gmail.com>
2024-06-27 21:54:47 -0500

    Se modifica el encriptador para que no acepte mayusculas

--- app.js
+++ app.js
@@ -14,26 +14,44 @@ function display() {
 
 function encriptarValor(){
     let valorTexto = cajaEntradaTexto.value
-    valorTexto.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
+    let arrayText = valorTexto.split(" ");
+    let resultadoEncriptado = [];
 
-    let textoVocales = valorTexto.replace(/[a]/g, "{a}")
-                                    .replace(/[e]/g, "{e}")
-                                    .replace(/[i]/g, "{i}")
-                                    .replace(/[o]/g, "{o}")
-                                    .replace(/[u]/g, "{u}")
+    for(let i = 0; i < arrayText.length; i++){
+        let palabra = arrayText[i];
+        let primeraPalabra = palabra.charAt(0);
+        if(primeraPalabra === primeraPalabra.toUpperCase()){
+            resultadoEncriptado.push(palabra);
+        }else{
+            let palabraNormalizada = palabra.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
 
-    let textoEncriptado = textoVocales.replace(/{a}/g, "ai")
-                                    .replace(/{e}/g, "enter")
-                                    .replace(/{i}/g, "imes")
-                                    .replace(/{o}/g, "ober")
-                                    .replace(/{u}/g, "ufat")
-   
-    console.log(cajaEntradaTexto.value);
-    console.log(textoEncriptado)
+            let textoVocales = palabraNormalizada.replace(/[a]/g, "{a}")
+                                                    .replace(/[e]/g, "{e}")
+                                                    .replace(/[i]/g, "{i}")
+                                                    .replace(/[o]/g, "{o}")
+                                                    .replace(/[u]/g, "{u}")
+
+            let palabraEncriptada = textoVocales.replace(/{a}/g, "ai")
+                                                .replace(/{e}/g, "enter")
+                                                .replace(/{i}/g, "imes")
+                                                .replace(/{o}/g, "ober")
+                                                .replace(/{u}/g, "ufat")
+            
+            resultadoEncriptado.push(palabraEncriptada);
+        }
+    }
     
-    parrafoSalidaTexto.innerHTML = textoEncriptado
-
+    
+    console.log(cajaEntradaTexto.value);
+    
+    let textoEncriptado = resultadoEncriptado.join(" ");
+    console.log(textoEncriptado);
+    parrafoSalidaTexto.innerHTML = textoEncriptado;
+    
     display();
+
+    return textoEncriptado;
+    
 }
 
 
